6e5547094f67fa8389e86cb11378b3488b5a9177
Bencece <zahoranbence@hotmail.com>
2021-05-20 21:43:41 +0200

    Added mongoose

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
+
+require('dotenv').config();
  
+var url = process.env.MONGODB
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>{
+    console.log("Csatlakoztunk!")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
-app.listen(3000)
+
+var port = process.env.PORT
+app.listen(port)
